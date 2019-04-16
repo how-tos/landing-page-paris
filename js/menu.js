d1251fb42b014dffaa51d575af25ff49d56ef3c6
coriagogo <pkcparis@gmail.com>
2019-04-16 13:38:51 -0400

    new style collapsing menu added and about page margins need to be updated

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,25 @@
-const toggleMenu = () => {
-    // Toggle the "menu--open" class on your menu refence. 
-    let element = document.querySelector('.menu')
-    element.classList.toggle('menu--open');
-    // element.classList.toggle('menu--open');
-  };
+// const toggleMenu = () => {
+//     // Toggle the "menu--open" class on your menu refence. 
+//     let element = document.querySelector('.menu')
+//     element.classList.toggle('menu--open');
+//     // element.classList.toggle('menu--open');
+//   };
   
-  // Start Here: Create a reference to the ".menu" class
-  const menu = document.querySelector('.menu');
-  // create a reference to the ".menu-button" class
-  const menuButton = document.querySelector('.menu-button');
-  // Using your menuButton reference, add a click handler that calls toggleMenu
-  menuButton.addEventListener('click', function() {
-    toggleMenu();
-  });
+//   // Start Here: Create a reference to the ".menu" class
+//   const menu = document.querySelector('.menu');
+//   // create a reference to the ".menu-button" class
+//   const menuButton = document.querySelector('.menu-button');
+//   // Using your menuButton reference, add a click handler that calls toggleMenu
+//   menuButton.addEventListener('click', function() {
+//     toggleMenu();
+//   });
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
